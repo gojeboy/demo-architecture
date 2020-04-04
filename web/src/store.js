07b2ab66874a9e545b204ad434f901b7cbd6cfe9
Njabulo Nsibande <njabsoul@gmail.com>
2020-04-04 18:16:40 +0200

    adding new venv on web

--- web/src/store.js
+++ web/src/store.js
@@ -5,11 +5,18 @@ import thunk from "redux-thunk";
 const initialState = {};
 const middleware = [thunk];
 
-export default createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const env = process.env.ENV;
+
+const compose_store = () => {
+  if (env.lowercase() == "dev") {
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+  } else {
+    compose(applyMiddleware(...middleware));
+  }
+};
+
+export default createStore(rootReducer, initialState, compose_store());
